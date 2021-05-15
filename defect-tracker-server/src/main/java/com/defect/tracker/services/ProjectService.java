717c565abc7d1587d2a88dad6a7316cbed771dfe
piruntha <pirunthapirunthy.com>
2021-05-15 15:48:19 -0700

    Merge branch 'Piruntha' of https://github.com/batch5-defect-tracker-project/Team02 into Piruntha

---
+++ defect-tracker-server/src/main/java/com/defect/tracker/services/ProjectService.java
@@ -0,0 +1,23 @@
+package com.defect.tracker.services;
+
+import java.util.List;
+
+import com.defect.tracker.data.entities.Project;
+
+	public interface ProjectService {
+
+	public boolean isProjectNameAlreadyExist(String projectName);
+
+	public void createProject(Project project);
+
+	public List<Project> getAllProject();
+
+	public void updateProject(Project project);
+
+	public boolean existsById(Long id);
+
+	public void deleteById(Long id);
+
+	public Object getProjectById(Long id);
+
+}
