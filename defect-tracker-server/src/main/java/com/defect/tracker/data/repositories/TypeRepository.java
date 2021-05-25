f31d984a5d30021041bf288940f085f19e698546
Nilucshiha <nilucshihasatchithananthan@gmail.com>
2021-05-25 17:02:59 +0530

    Changes

--- defect-tracker-server/src/main/java/com/defect/tracker/data/repositories/TypeRepository.java
+++ defect-tracker-server/src/main/java/com/defect/tracker/data/repositories/TypeRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.defect.tracker.data.entities.Type;
 
-
-
 public interface TypeRepository extends JpaRepository<Type, Long>{
 
 	boolean existsByName(String name);
